021b6cb403fccf5024b8473e2de694a151b6a225
YugAjmera <yugajmera@gmail.com>
2019-02-28 19:13:03 +0530

    Update

--- teleop_keyboard_omni3.py
+++ teleop_keyboard_omni3.py
@@ -2,10 +2,9 @@
 
 from __future__ import print_function
 
-import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
 
-from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
 
 import sys, select, termios, tty
 
@@ -29,82 +28,72 @@ b : down (-z)
 anything else : stop
 
 q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
 
 CTRL-C to quit
 """
 
 moveBindings = {
-        'i':(1,0,0,0),
-        'o':(1,0,0,-1),
-        'j':(0,0,0,1),
-        'l':(0,0,0,-1),
-        'u':(1,0,0,1),
-        ',':(-1,0,0,0),
-        '.':(-1,0,0,1),
-        'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
-        'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
-        '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
-        't':(0,0,1,0),
-        'b':(0,0,-1,0),
+        'i':(0,-1,1),
+        'o':(1,-1,0),
+        'j':(1,1,1),
+        'l':(-1,-1,-1),
+        'u':(-1,0,1),
+        ',':(0,1,-1),
+        '.':(1,0,-1),
+        'm':(-1,1,0),  
+        'O':(1,-1,0),
+        'I':(0,-1,1),
+        'J':(1,1,1),
+        'L':(-1,0,1),
+        'U':(-1,0,1),
+        '<':(0,1,-1),
+        '>':(1,0,-1),
+        'M':(-1,1,0),  
     }
 
 speedBindings={
         'q':(1.1,1.1),
         'z':(.9,.9),
-        'w':(1.1,1),
-        'x':(.9,1),
-        'e':(1,1.1),
-        'c':(1,.9),
     }
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
-    select.select([sys.stdin], [], [], 0)
     key = sys.stdin.read(1)
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
 
-def vels(speed,turn):
-    return "currently:\tspeed %s\tturn %s " % (speed,turn)
+def vels(speed):
+    return "currently:\tspeed %s " % (speed)
 
 if __name__=="__main__":
     settings = termios.tcgetattr(sys.stdin)
 
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
-    rospy.init_node('teleop_twist_keyboard')
+    rospy.init_node('vel_Publisher')
+    pub = rospy.Publisher('/open_base/back_joint_velocity_controller/command', Float64, queue_size=1)
+    publ = rospy.Publisher('/open_base/left_joint_velocity_controller/command', Float64, queue_size=1)
+    pubr = rospy.Publisher('/open_base/right_joint_velocity_controller/command', Float64, queue_size=1)
 
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
+
+    speed = 1.0
     x = 0
     y = 0
     z = 0
-    th = 0
     status = 0
 
     try:
         print(msg)
-        print(vels(speed,turn))
+        print(vels(speed))
         while(1):
             key = getKey()
             if key in moveBindings.keys():
                 x = moveBindings[key][0]
                 y = moveBindings[key][1]
                 z = moveBindings[key][2]
-                th = moveBindings[key][3]
             elif key in speedBindings.keys():
                 speed = speed * speedBindings[key][0]
-                turn = turn * speedBindings[key][1]
-
-                print(vels(speed,turn))
+       
+                print(vels(speed))
                 if (status == 14):
                     print(msg)
                 status = (status + 1) % 15
@@ -116,18 +105,32 @@ if __name__=="__main__":
                 if (key == '\x03'):
                     break
 
-            twist = Twist()
-            twist.linear.x = x*speed; twist.linear.y = y*speed; twist.linear.z = z*speed;
-            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th*turn
-            pub.publish(twist)
+            vel = Float64()
+	    vell = Float64()
+	    velr = Float64()
+	
+	    vel = x*speed
+	    vell = y*speed
+	    velr = z*speed
+
+	    pub.publish(vel)
+	    publ.publish(vell)
+	    pubr.publish(velr)
 
     except Exception as e:
         print(e)
 
     finally:
-        twist = Twist()
-        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
-        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
-        pub.publish(twist)
+        vel = Float64()
+	vell = Float64()
+	velr = Float64()
+	
+	vel = 0.0
+	vell = 0.0
+	velr = 0.0
+
+	pub.publish(vel)
+	publ.publish(vell)
+	pubr.publish(velr)
 
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
